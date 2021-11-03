07260abd741f7a7a4dc1c5cb134912124afd32a4
Genne Liu <genneliu@gmail.com>
2021-11-03 09:52:06 -0700

    debug and chalk

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const express = require('express');
+// chalk allows us to set colors on messages
+const chalk = require('chalk');
+const debug = require('debug')('app');
 
 // need instance of express, don't short circuit
 const app = express();
@@ -6,12 +9,16 @@ const app = express();
 
 // list of requires
 // express is series of route -> function 
+
+
 app.get('/', (req, res) => {
     res.send("Hello from my app")
 } )
 
-// listen on port with callback
+/* listen on port with callback
+ dont want to use console.log, use debug instead
+ debug only runs in debug mode */
 app.listen(3000, ()=> {
-    console.log("listening on port 3000")
+    debug(`shhh its running on ${chalk.greenBright('3000')}`)
 })
 
